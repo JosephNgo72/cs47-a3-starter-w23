56f2bda633e8a4695ab11cfbc19e4429b0007035
Joseph Ngo <67983189+JosephNgo72@users.noreply.github.com>
2023-02-17 23:48:00 -0800

    done
    done

--- components/SongItem.js
+++ components/SongItem.js
@@ -13,10 +13,12 @@ const SongItem = ({
     <View style={styles.item}>
       <Text style={styles.index}>{index}</Text>
       <Image source={{ uri: image }} style={styles.image} />
-      <Text numberOfLines={1} style={styles.songTitle}>
-        {songTitle}
-      </Text>
-      <Text style={styles.artist}>{artist}</Text>
+      <View style="song">
+        <Text numberOfLines={1} style={styles.songTitle}>
+          {songTitle}
+        </Text>
+        <Text style={styles.artist}>{artist}</Text>
+      </View>
       <Text numberOfLines={1} style={styles.songAlbum}>
         {songAlbum}
       </Text>
@@ -43,8 +45,11 @@ const styles = StyleSheet.create({
     margin: 5,
     resizeMode: "contain",
   },
-  songTitle: { width: "30%", color: "white" },
-  artist: { width: "15%", color: "white" },
+  songTitle: { width: 160, color: "white" },
+  artist: { color: "white" },
+  song: {
+    width: "45%",
+  },
   songAlbum: { width: "15%", color: "white" },
   duration: { width: "10%", color: "white" },
 });
